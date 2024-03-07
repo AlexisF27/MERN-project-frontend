8d9ed5ffc7252362fa2d3f1c84029c3b206fd59b
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-03-07 21:06:19 +0000

    App with a route to dash /

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
 
+import Layout from './components/Layout'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+
+      </Route>{/* End Dash */}
+
+
+    </Routes>
+
   )
 }
 
