e442baf76480050916a46f5880ba0de50267a726
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-03-07 23:57:29 +0000

    Base strucutre

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 
+import DashLayout from './components/DashLayout'
 import Layout from './components/Layout'
-import Login from './components/Login';
+import Login from './features/auth/Login'
+import NotesList from './features/notes/NotesList'
 import Public from './components/Public'
+import UsersList from './features/users/UsersList'
+import Welcome from './features/auth/Welcome'
 
 function App() {
 
@@ -12,6 +16,18 @@ function App() {
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
 
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+          
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+
+        </Route>
 
 
       </Route>{/* End Dash */}
---
+++ src/features/notes/NotesList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NotesList() {
+  return (
+    <div>NotesList</div>
+  )
+}
+
+export default NotesList
