7230c48eb44baf821dd06c79df2cca45bc30367a
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-03-07 20:51:09 +0000

    Addin router to the main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import App from './App.jsx'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 )
