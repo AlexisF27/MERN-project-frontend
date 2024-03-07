dcc0c4d425ecc4d72258f0342b782404cd1c5415
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-03-07 22:20:51 +0000

    DashLayout - DashHeader - DashFooter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
 
 import Layout from './components/Layout'
+import Login from './components/Login';
+import Public from './components/Public'
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+
+
 
       </Route>{/* End Dash */}
 
