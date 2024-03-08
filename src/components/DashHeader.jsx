144ea2fcc92d51bd05ede1265f2564a4ebf93a9a
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-03-08 00:02:30 +0000

    Bug fix on DashHeader

--- src/components/DashHeader.jsx
+++ src/components/DashHeader.jsx
@@ -1,35 +1,20 @@
-import { useLocation, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHouse } from "@fortawesome/free-solid-svg-icons"
-
-const DashFooter = () => {
-
-    const navigate = useNavigate()
-    const { pathname } = useLocation()
-
-    const onGoHomeClicked = () => navigate('/dash')
-
-    let goHomeButton = null
-    if (pathname !== '/dash') {
-        goHomeButton = (
-            <button
-                className="dash-footer__button icon-button"
-                title="Home"
-                onClick={onGoHomeClicked}
-            >
-                <FontAwesomeIcon icon={faHouse} />
-            </button>
-        )
-    }
+const DashHeader = () => {
 
     const content = (
-        <footer className="dash-footer">
-            {goHomeButton}
-            <p>Current User:</p>
-            <p>Status:</p>
-        </footer>
+        <header className="dash-header">
+            <div className="dash-header__container">
+                <Link to="/dash">
+                    <h1 className="dash-header__title">techNotes</h1>
+                </Link>
+                <nav className="dash-header__nav">
+                    {/* add nav buttons later */}
+                </nav>
+            </div>
+        </header>
     )
+
     return content
 }
-export default DashFooter
+export default DashHeader
