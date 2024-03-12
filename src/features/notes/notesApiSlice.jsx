c82cc9bd262345e834537b6118a4fc8a42260dd8
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-03-12 19:54:43 +0000

    NotesApiSlice - add methods for Post/Patch/Delete

--- src/features/notes/notesApiSlice.jsx
+++ src/features/notes/notesApiSlice.jsx
@@ -13,37 +13,70 @@ const initialState = notesAdapter.getInitialState()
 
 export const notesApiSlice = apiSlice.injectEndpoints({
   endpoints: builder => ({
-    getNotes: builder.query({
-      query: () => '/notes',
-      validateStatus: (response, result) => {
-        return response.status === 200 && !result.isError
-      },
-      keepUnusedDataFor: 5,
-      transformResponse: responseData => {
-        console.log("-----");
-        const loadedNotes = responseData.map(note => {
-          console.log("-----");
-
-          note.id = note._id
-          return note
-        });
-        return notesAdapter.setAll(initialState, loadedNotes)
-      },
-      // eslint-disable-next-line no-unused-vars
-      providesTags: (result, error, arg) => {
-        if (result?.ids) {
-          return [
-            { type: 'Note', id: 'LIST' },
-            ...result.ids.map(id => ({ type: 'Note', id }))
+      getNotes: builder.query({
+          query: () => '/notes',
+          validateStatus: (response, result) => {
+              return response.status === 200 && !result.isError
+          },
+          transformResponse: responseData => {
+              const loadedNotes = responseData.map(note => {
+                  note.id = note._id
+                  return note
+              });
+              return notesAdapter.setAll(initialState, loadedNotes)
+          },
+          // eslint-disable-next-line no-unused-vars
+          providesTags: (result, error, arg) => {
+              if (result?.ids) {
+                  return [
+                      { type: 'Note', id: 'LIST' },
+                      ...result.ids.map(id => ({ type: 'Note', id }))
+                  ]
+              } else return [{ type: 'Note', id: 'LIST' }]
+          }
+      }),
+      addNewNote: builder.mutation({
+          query: initialNote => ({
+              url: '/notes',
+              method: 'POST',
+              body: {
+                  ...initialNote,
+              }
+          }),
+          invalidatesTags: [
+              { type: 'Note', id: "LIST" }
           ]
-        } else return [{ type: 'Note', id: 'LIST' }]
-      }
-    }),
+      }),
+      updateNote: builder.mutation({
+          query: initialNote => ({
+              url: '/notes',
+              method: 'PATCH',
+              body: {
+                  ...initialNote,
+              }
+          }),
+          invalidatesTags: (result, error, arg) => [
+              { type: 'Note', id: arg.id }
+          ]
+      }),
+      deleteNote: builder.mutation({
+          query: ({ id }) => ({
+              url: `/notes`,
+              method: 'DELETE',
+              body: { id }
+          }),
+          invalidatesTags: (result, error, arg) => [
+              { type: 'Note', id: arg.id }
+          ]
+      }),
   }),
 })
 
 export const {
   useGetNotesQuery,
+  useAddNewNoteMutation,
+  useUpdateNoteMutation,
+  useDeleteNoteMutation,
 } = notesApiSlice
 
 // returns the query result object
