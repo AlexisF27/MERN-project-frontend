ffec89eb4e0e65c29c996fb0770a45e8182ba901
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-03-12 20:03:28 +0000

    Add to App routes for the placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 
 import DashLayout from './components/DashLayout'
+import EditNote from './features/notes/EditNote'
+import EditUser from './features/users/EditUser'
 import Layout from './components/Layout'
 import Login from './features/auth/Login'
+import NewNote from './features/notes/NewNote'
+import NewUserForm from './features/users/NewUserForm'
 import NotesList from './features/notes/NotesList'
 import Public from './components/Public'
 import UsersList from './features/users/UsersList'
@@ -18,14 +22,19 @@ function App() {
 
         <Route path="dash" element={<DashLayout />}>
           <Route index element={<Welcome />} />
-          
-          <Route path="notes">
-            <Route index element={<NotesList />} />
-          </Route>
 
           <Route path="users">
             <Route index element={<UsersList />} />
+            <Route path=":id" element={<EditUser />} />
+            <Route path="new" element={<NewUserForm />} />
           </Route>
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+            <Route path=":id" element={<EditNote />} />
+            <Route path="new" element={<NewNote />} />
+          </Route>
+
 
         </Route>
 
